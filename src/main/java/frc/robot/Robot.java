f624e974388ea5e986666b7f600596ff56ee09cf
Sean Walberg <sean@ertw.com>
2019-12-03 21:33:15 -0500

    First commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+  private WPI_TalonSRX left, right;
+  private Joystick m_leftStick;
+  private Joystick m_rightStick;
+
+  @Override
+  public void robotInit() {
+    left = new WPI_TalonSRX(4);
+    right = new WPI_TalonSRX(5);
+    m_myRobot = new DifferentialDrive(left, right);
+    m_leftStick = new Joystick(0);
+    m_rightStick = new Joystick(1);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    System.out.printf("Position is %d velocity is %d\n", left.getSensorCollection().getQuadraturePosition(), left.getSensorCollection().getQuadratureVelocity());
+  } 
+}
